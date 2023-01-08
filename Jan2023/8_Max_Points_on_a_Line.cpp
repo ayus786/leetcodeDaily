dda372d1c435121af8cad5a4f7a3daaeb9ceb1d2
ayush <47188199+ayus786@users.noreply.github.com>
2023-01-08 23:58:39 +0530

    Update 8_Max_Points_on_a_Line.cpp

--- Jan2023/8_Max_Points_on_a_Line.cpp
+++ Jan2023/8_Max_Points_on_a_Line.cpp
@@ -18,3 +18,28 @@ public:
         return maxi;
     }
 };
+
+// optimized
+#define INF 1000000
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& pt) {
+        int ans=1; 
+        int n=pt.size();
+        for(int i=0;i<n-1;i++){
+            map<double,int> mp;
+            for(int j=i+1;j<n;j++){
+                double x = (double)(pt[j][1]-pt[i][1])/(double)(pt[j][0]-pt[i][0]);
+                if((pt[j][0]-pt[i][0])==0 )
+                    mp[INF]++; 
+                else
+                    mp[x]++;
+            }
+            int temp = 0;
+            for(auto it:mp)
+                temp = max(temp, it.second+1);
+            ans = max(temp, ans);
+        }
+        return ans;
+    }
+};
